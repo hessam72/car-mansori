9f0b7d86cdffce3061bc3ac95d5044fff2165ccc
Hesam Tahami <hessamtahani@gmail.com>
2026-07-04 12:04:58 +0330

    Update ReflectiveFloor.tsx

--- components/store/ReflectiveFloor.tsx
+++ components/store/ReflectiveFloor.tsx
@@ -17,7 +17,7 @@ export function ReflectiveFloor({
   return (
     <mesh
       rotation={[-Math.PI / 2, 0, 0]}
-      position={[0, .42, 0]}
+      position={[0, -0.8, 0]}
       receiveShadow
     >
       <planeGeometry args={[size, size]} />
