0a0a1ea45cbbf0c3084a84c5f11e48a3b2d426b1
Hesam Tahami <hessamtahani@gmail.com>
2026-07-25 12:24:33 +0330

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "vto-store",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3021",
+      args: "start -p 3020",
       exec_mode: "fork",
       instances: 1,
       env: {
